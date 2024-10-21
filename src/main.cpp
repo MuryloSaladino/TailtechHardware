5978e66faf4455158b23d5a983a39e6e30a71408
MuryloSaladino <murylosaladino@gmail.com>
2024-10-21 09:47:42 -0300

    register door

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,85 @@
+// --------------------------- Libs ---------------------------
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <string>
+#include <WiFiClient.h>
 
-// Replace these with your network credentials
-const char *ssid = "Kauane";
-const char *password = "27030476";
 
-WiFiClient client;
+// ------------------ Functions declarations ------------------
+void connectToWiFi();
+void registerEsp();
 
-String macAddress;
+// --------------------- Global Constants ---------------------
 
+// --------------------- Global Variables ---------------------
+String ssid = "Murylão";
+String password = "playsdomurylo";
+String token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mjk0Nzc0NzAsInN1YiI6IjE4MmFiZDNlLWE3YTktNDNkYi1hYmQ5LTg3YzUzMDkyODczOSJ9.uD5XCfF-Fvh9YUebnVF17_WeVZf2qMPZ2FAovsecMUM";
+String espToken = "";
+
+
+// --------------------------- Setup --------------------------
 void setup()
 {
-    macAddress = WiFi.macAddress();
-
     Serial.begin(115200);
-
-    // Connect to WiFi
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED);
-
-    // Send HTTP request after WiFi connection
-    if (WiFi.status() == WL_CONNECTED)
-    {
-        HTTPClient http;
-
-        // Specify the URL and pass the WiFiClient object
-        String url = "http://192.168.0.104:3000/api/esp/" + macAddress;
-        http.begin(client, url); // Replace with your server URL
-
-        // Send HTTP GET request
-        int httpCode = http.GET();
-
-        // Check the returning code
-        if (httpCode > 0)
-        {
-            Serial.printf("GET... code: %d\n", httpCode);
-
-            // File found at server
-            if (httpCode == HTTP_CODE_OK)
-            {
-                String payload = http.getString();
-                Serial.println("Response:");
-                Serial.println(payload);
-            }
-        }
-        else
-        {
-            Serial.printf("GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-
-        http.end(); // Free resources
-    }
 }
 
+
+// --------------------------- Loop ---------------------------
 void loop()
 {
-    Serial.println(macAddress);
+    // If user has already sent token with wifi info
+    if(token.length() > 0) 
+    {
+        // Connect to WiFi
+        if(WiFi.status() != WL_CONNECTED) 
+            connectToWiFi();
+
+        // Send door register request in case you don't have the token already
+        if(espToken.length() == 0)
+        {
+            registerEsp();
+            Serial.print("Token: ");
+            Serial.println(espToken);
+        }
+    }
+
+
+}
+
+void connectToWiFi() {
+    WiFi.begin(ssid, password);
+    Serial.print("\nConnecting to Wi-Fi");
+
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.print("\nWiFi connected. IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void registerEsp() {
+    HTTPClient http;
+    WiFiClient wifiClient;
+    http.begin(wifiClient, "http://192.168.164.137:3000/api/esp");
+
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("Authorization", token);
+
+    int response = http.POST("");
+    if(response == 201) 
+    {
+        espToken = http.getString();
+    }
+    else
+    {
+        Serial.println(response);
+        Serial.println(http.getString());
+    }
+
+    http.end();
 }
