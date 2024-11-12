5cd893230339326a5d104f1ecf943066071f26d8
MuryloSaladino <murylosaladino@gmail.com>
2024-11-12 08:32:14 -0300

    manual setup

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,20 @@
 #include <Servo.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <ESP8266WebServer.h>
+#include <WiFiClientSecure.h>
+#include <EEPROM.h>
 
 
 // ------------------ Functions declarations ------------------
-void connectToWiFi();
-void registerEsp();
+void connectToWiFi(String ssid, String password);
+void registerEsp(String userToken);
 bool verifyTag(String UID);
 String getTagUID(MFRC522 mfrc522);
+void handleRoot();
+void handleLogin();
+String getUserToken(String email, String password);
+void startAP();
 
 // --------------------- Global Constants ---------------------
 #define SS_PIN D2
@@ -21,15 +28,13 @@ String getTagUID(MFRC522 mfrc522);
 #define SERVO_PIN D4
 
 // --------------------- Global Variables ---------------------
-String ssid = "Murylão";
-String password = "playsdomurylo";
-
-String token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3Mjk0Nzc0NzAsInN1YiI6IjE4MmFiZDNlLWE3YTktNDNkYi1hYmQ5LTg3YzUzMDkyODczOSJ9.uD5XCfF-Fvh9YUebnVF17_WeVZf2qMPZ2FAovsecMUM";
 String espToken = "";
 
 Servo doorLock;
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
+ESP8266WebServer server(80);
+const char* apSSID = "DeviceSetup";
 
 // --------------------------- Setup --------------------------
 void setup()
@@ -41,26 +46,19 @@ void setup()
 
     doorLock.attach(SERVO_PIN); 
     doorLock.write(0);
+
+    startAP();
 }
 
 
 // --------------------------- Loop ---------------------------
 void loop()
 {
-    // If user has already sent token with wifi info
-    if(token.length() > 0) 
+    // await for user to setup through AP Mode
+    if(espToken.length() == 0)
     {
-        // Connect to WiFi
-        if(WiFi.status() != WL_CONNECTED) 
-            connectToWiFi();
-
-        // Send door register request in case you don't have the token already
-        if(espToken.length() == 0)
-        {
-            registerEsp();
-            Serial.print("Token: ");
-            Serial.println(espToken);
-        }
+        server.handleClient();
+        return;
     }
 
     // Look for new RFID cards+
@@ -71,17 +69,23 @@ void loop()
         return;
     
     String tagUID = getTagUID(mfrc522);
+    Serial.println(tagUID);
 
     if(verifyTag(tagUID))
     {
-        delay(2000);
         doorLock.write(90);
-        delay(2000);
+        delay(5000);
         doorLock.write(0);
+    }
+    else
+    {
+        delay(1000);
     }
 }
 
-void connectToWiFi() {
+void connectToWiFi(String ssid, String password) 
+{
+    WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     Serial.print("\nConnecting to Wi-Fi");
 
@@ -95,18 +99,20 @@ void connectToWiFi() {
     Serial.println(WiFi.localIP());
 }
 
-void registerEsp() {
+void registerEsp(String userToken) 
+{
     HTTPClient http;
     WiFiClient wifiClient;
-    http.begin(wifiClient, "http://192.168.164.137:3000/api/esp");
+    http.begin(wifiClient, "http://192.168.0.105:3000/api/esp");
 
     http.addHeader("Content-Type", "application/json");
-    http.addHeader("Authorization", token);
+    http.addHeader("Authorization", userToken);
 
     int response = http.POST("");
     if(response == 201) 
     {
         String body = http.getString();
+        Serial.println(body);
         String res = body.substring(1, body.length() - 1);
         espToken = "Bearer " + res;
     }
@@ -114,11 +120,12 @@ void registerEsp() {
     http.end();
 }
 
-bool verifyTag(String UID) {
+bool verifyTag(String UID) 
+{
     HTTPClient http;
     WiFiClient wifiClient;
 
-    String endpoint = "http://192.168.164.137:3000/api/esp/pet/" + UID;
+    String endpoint = "http://192.168.0.105:3000/api/esp/pet/" + UID;
     http.begin(wifiClient, endpoint);
 
     http.addHeader("Content-Type", "application/json");
@@ -133,7 +140,8 @@ bool verifyTag(String UID) {
     return response == 204;
 }
 
-String getTagUID(MFRC522 mfrc522) {
+String getTagUID(MFRC522 mfrc522) 
+{
     String uidString = "";
     for (byte i = 0; i < mfrc522.uid.size; i++) {
         if (mfrc522.uid.uidByte[i] < 0x10) {
@@ -144,3 +152,64 @@ String getTagUID(MFRC522 mfrc522) {
     uidString.toUpperCase();
     return uidString;
 }
+
+void startAP() 
+{
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(apSSID);
+    IPAddress IP = WiFi.softAPIP();
+    Serial.println("AP mode started");
+    Serial.print("Server IP address: ");
+    Serial.println(IP);
+
+    server.on("/", handleRoot);
+    server.on("/login", handleLogin);
+    server.begin();
+}
+
+void handleRoot() 
+{
+    String htmlPage = "<form action='/login' method='POST'>"
+                      "WiFi SSID: <input name='ssid'><br>"
+                      "WiFi Password: <input name='wifiPassword' type='password'><br>"
+                      "Email: <input name='email'><br>"
+                      "Password: <input name='password' type='password'><br>"
+                      "<input type='submit' value='Submit'>"
+                      "</form>";
+    server.send(200, "text/html", htmlPage);
+}
+
+void handleLogin() 
+{
+    String form_ssid = server.arg("ssid");
+    String form_wifi_password = server.arg("wifiPassword");
+    String form_email = server.arg("email");
+    String form_password = server.arg("password");
+
+    connectToWiFi(form_ssid, form_wifi_password);
+    String userToken = getUserToken(form_email, form_password);
+    registerEsp(userToken);
+}
+
+String getUserToken(String email, String password)
+{
+    HTTPClient http;
+    WiFiClient wifiClient;
+
+    String endpoint = "http://192.168.0.105:3000/api/login";
+    String payload = "{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}";
+    http.begin(wifiClient, endpoint);
+    http.addHeader("Content-Type", "application/json");
+
+    int responseCode = http.POST(payload);
+    String responseBody = http.getString();
+
+    Serial.println(responseCode);
+    Serial.println(responseBody);
+
+    int tokenStart = responseBody.indexOf("token\":\"") + 8;
+    int tokenEnd = responseBody.indexOf("\"", tokenStart);
+    http.end();
+
+    return "Bearer " + responseBody.substring(tokenStart, tokenEnd);
+} 
